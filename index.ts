f082f11ed2468849bf73571f28d7c4e218bff323
Anwesh43 <anweshthecool0@gmail.com>
2020-10-08 13:53:23 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -28,3 +28,48 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x1, y1)
+        context.stroke()
+    }
+
+    static drawSlopeDownFill(context : CanvasRenderingContext2D, sf4 : number, x : number, y : number, offset : number) {
+        context.beginPath()
+        context.moveTo(0, 0)
+        context.lineTo(offset, y)
+        context.lineTo(x, y)
+        context.lineTo(w, 0)
+        context.lineTo(w, h)
+        context.lineTo(0, h)
+        context.lineTo(0, 0)
+        context.clip()
+        context.fillRect(0, 0, w * sf4, h)
+    }
+
+    static drawSlopeDown(context : CanvasRenderingContext2D, scale : number) {
+        const offset = Math.min(w, h) / sizeFactor
+        const y : number = h - offset  
+        const x : number = w - offset 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        DrawingUtil.drawLine(context, 0, 0, offset * sf1, y * sf1)
+        DrawingUtil.drawLine(context, offset, y, offset + (x - offset) * sf2, y)
+        DrawingUtil.drawLine(context, x, y, x + (w - x) * sf3, y - y * sf3)
+    }
+
+    static drawSDFNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawSlopeDown(context, scale)
+    }
+}
