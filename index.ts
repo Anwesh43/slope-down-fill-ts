d377ea863bdc3b228b19df53e827075f34902805
Anwesh43 <anweshthecool0@gmail.com>
2020-10-08 14:19:39 +0530

    adding SlopeDown class

--- index.ts
+++ index.ts
@@ -190,3 +190,26 @@ class SDFNode {
         return this 
     }
 }
+
+class SlopeDown {
+
+    curr : SDFNode = new SDFNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
